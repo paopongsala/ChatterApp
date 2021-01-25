480d1daa2e98933d0b92fecaf3d3a2240dbec5d4
paopongsala <pao@PaoMac.fully.qualified.domain>
2021-01-25 12:45:18 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+function App() {
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+      CHATTER
+    </header>
+  </div>
+}
+export default App;

34ea4cb3e550fee38957a051f2d6f0ec73f8c3ce
paopongsala <pao@PaoMac.fully.qualified.domain>
2021-01-28 17:37:48 -0800

    added styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css';
-
-
+import TextInput from './TextInput';
 function App() {
+  const [messages,setMessages] = useState([{text:'hello'}])
   return <div className="App">
     <header className="header">
       <div className="logo" />
-      CHATTER
+      Chatter
+      
     </header>
+    <div className="message">
+      {messages[0].text}
+    </div>
+    <TextInput 
+      send={(t)=> setMessages([{text:t}])}
+    />
   </div>
 }
 export default App;
---
+++ src/TextInput.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+function TextInput(props) {
+  const [text, setText] = useState('')
+
+  function sendMessage(){
+    if(text==='') return  
+    props.send(text)
+    setText('')
+  }
+
+  function keyPressed(e){
+      if(e.key==='Enter'){
+          sendMessage()
+      }
+  }
+
+  return <footer className="text-input">
+      <input className="textbox"
+      placeholder="Write your message"
+      value={text}
+      onChange={e=> setText(e.target.value)}
+      onKeyPress={keyPressed}
+    />
+    <button className="send-button" onClick={sendMessage}>➤</button>
+  </footer>
+}
+
+export default TextInput

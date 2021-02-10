ac923f8f97fe8fe5bee7763bb91f8438177bf900
paopongsala <pao@PaoMac.fully.qualified.domain>
2021-02-10 10:26:23 -0800

    add route

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import TextInput from './TextInput';
 import Message from './Message.js'
 import NamePicker from './NamePicker.js'
 import {db, useDB} from './db'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  const messages = useDB()
+function Wrap() {
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/:room" component={App} />
+    </Switch>
+  </BrowserRouter>
+}
+
+
+function App(props) {
+  console.log(props)
+  const room = props.match.params.room || 'home'
+  console.log("CHAT ROOM", room)
+
+  const messages = useDB(room)
   const [username, setUsername] = useState(
     localStorage.getItem('username') || 'Set username:')
 
@@ -26,8 +41,8 @@ function App() {
     </main>
 
     <TextInput 
-      send={(t)=> db.send({text:t, name:username, date:new Date()})}
+      send={(t)=> db.send({text:t, name:username, date:new Date(), room})}
     />
   </div>
 }
-export default App;
+export default Wrap;

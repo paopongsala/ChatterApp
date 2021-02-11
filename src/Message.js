c6b8db35e70ff8b9208537d779e9dc36c5e3ad42
paopongsala <pao@PaoMac.fully.qualified.domain>
2021-02-11 06:11:55 -0800

    username

--- src/Message.js
+++ src/Message.js
@@ -4,10 +4,11 @@ function Message(props){
 
   return <div className="message-row"
     style={{justifyContent: props.isMe ? 'flex-end' : 'flex-start'}}>
-  <div className="message">
-    <div className="my-name-header">{props.name}</div>
-    {props.text}
-  </div>
+      
+      <div className="message">
+        <div className="my-name-header">{props.name}</div>
+        {props.text}
+      </div>
 </div>
 }
 export default Message
